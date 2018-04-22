54725648a860f32619756cf8a5393c97675f39e6
zhushengjun <1185335441@qq.com>
2018-04-22 20:07:06 +0800

    改一处而动全身

---
+++ app/src/main/java/com/zsj/dragger2demo/BackupManager.java
@@ -0,0 +1,16 @@
+package com.zsj.dragger2demo;
+
+/**
+ * @author 朱胜军
+ * @date 2018/4/22
+ * 描述	      TODO
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   TODO
+ */
+public class BackupManager {
+    public BackupManager() {
+        
+    }
+}
---
+++ app/src/main/java/com/zsj/dragger2demo/RemoteBackup.java
@@ -0,0 +1,13 @@
+package com.zsj.dragger2demo;
+
+/**
+ * @author 朱胜军
+ * @date 2018/4/22
+ * 描述	      TODO
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   TODO
+ */
+public class RemoteBackup {
+}
